4f39297b869bd246d631a1196e6a3d51bb69dac4
SolCaruso <caruso@ocelot.ltd>
2025-08-22 02:28:01 -0400

    updates bio to be a bit less cringe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           </h1>
           <p className="text-3xl font-regular font-cormorant-garamond text-vacchio-black/70 pb-4">Software Developer</p>
           <p className="text-base font-regular text-vacchio-black/50 leading-relaxed max-w-sm">
-            Passionate about building software that combines thoughtful design with robust engineering.
+            Passionate about creating thoughtfully designed, performant interfaces. 
           </p>
         </div>
         <Pagination />
@@ -36,7 +36,7 @@ export default function Home() {
             </h1>
             <p className="text-2xl sm:text-3xl font-regular font-cormorant-garamond text-vacchio-black/70 pb-4">Software Developer</p>
             <p className="text-base font-regular text-vacchio-black/50 leading-relaxed max-w-sm">
-              Passionate about building software that combines thoughtful design with robust engineering.
+              Passionate about creating thoughtfully designed, performant interfaces. 
             </p>
             <div className="mt-8">
               <SocialLinks />
